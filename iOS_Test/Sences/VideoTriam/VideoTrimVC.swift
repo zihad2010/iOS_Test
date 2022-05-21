7d479c476dfcf00ae8fb5d06374ed018c3bef39d
Asraful <asrafulalam2010502cse@gmail.com>
2022-05-21 12:10:44 +0600

    code refactor.

--- iOS_Test/Sences/VideoTriam/VideoTrimVC.swift
+++ iOS_Test/Sences/VideoTriam/VideoTrimVC.swift
@@ -42,22 +42,13 @@ class VideoTrimVC: UIViewController {
             return
         }
         videoRangeSlider.setVideoURL(videoURL:url)
-        
         videoRangeSlider.delegate = self
         videoRangeSlider.minSpace = 1.0
-        //videoRangeSlider.maxSpace = 180.0
-        
-        //   lblMinSpace.text = "\(videoRangeSlider.minSpace)"
-        
         videoRangeSlider.setStartPosition(seconds: 0.0)
         self.startIndicatorLebel.text = STARTVALUE.addString(UNIT)
-        
         videoRangeSlider.setEndPosition(seconds: 5.0)
         self.endIndicatorLebel.text = ENDVALUE.addString(UNIT)
         
-        // videoRangeSlider.startTimeView.marginLeft = 5.0
-        // videoRangeSlider.startTimeView.marginRight = 5.0
-        videoRangeSlider.startTimeView.timeLabel.textColor = .white
     }
     
     private func setUpBinding(){
@@ -82,7 +73,7 @@ class VideoTrimVC: UIViewController {
 
 extension VideoTrimVC: ABVideoRangeSliderDelegate{
     
-    // MARK: ABVideoRangeSlider Delegate - Returns time in seconds
+    // MARK: VideoRangeSlider Delegate - Returns time in seconds
     
     func didChangeValue(videoRangeSlider: ABVideoRangeSlider, startTime: Float64, endTime: Float64) {
         self.startIndicatorLebel.text = startTime.float64ToString().addString(UNIT)
